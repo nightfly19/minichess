0a60405ab65c863151241964f1ae9c5c9b5e6a45
Sage Imel <sage@sagenite.net>
2013-05-01 01:00:33 -0700

    Mostly done with new thing.

--- elo100.lisp
+++ elo100.lisp
@@ -6,11 +6,11 @@
 (load "pieces.lisp") ;;works
 (load "coords.lisp") ;;works
 (load "game-state.lisp") ;;seems to work
-(load "possible-moves.lisp") ;;works
-(load "game-status.lisp") ;;works
-(load "transposition-table.lisp") ;;works
-(load "bot.lisp") ;;works
-(load "external.lisp") ;;works?
+;;(load "possible-moves.lisp") ;;works
+;;(load "game-status.lisp") ;;works
+;;(load "transposition-table.lisp") ;;works
+;;(load "bot.lisp") ;;works
+;;(load "external.lisp") ;;works?
 ;;(load "play.lisp") ;;works?
-(load "imcs.lisp") ;;works
-(load "tests.lisp") ;;works, not not good enough :/
+;;(load "imcs.lisp") ;;works
+;;(load "tests.lisp") ;;works, not not good enough :/
