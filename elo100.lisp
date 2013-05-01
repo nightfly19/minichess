359cf681d0ee16f055b923f5085d75bd74b33b53
Sage Imel <sage@sagenite.net>
2013-05-01 01:49:11 -0700

    score cache updated but not tested

--- elo100.lisp
+++ elo100.lisp
@@ -8,7 +8,7 @@
 (load "game-state.lisp") ;;seems to work
 (load "possible-moves.lisp") ;;works
 (load "game-status.lisp") ;;works
-;;(load "transposition-table.lisp") ;;works
+(load "transposition-table.lisp") ;;works
 ;;(load "bot.lisp") ;;works
 ;;(load "external.lisp") ;;works?
 ;;(load "play.lisp") ;;works?
